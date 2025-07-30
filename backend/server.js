c63daaae044213e7484038f2818ce6a9b3a2ce03
Amitkm997 <amitkm997@gmail.com>
2025-07-30 22:03:28 +0530

    mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,76 +1,23 @@
 import express from 'express';
 import morgan from 'morgan';
-
+import userRoute from './routes/userRoute.js'
+import mongoose from 'mongoose';
 const app=express();// initizilise the express
 const PORT=5000;
 
 app.use(express.json())
 app.use(morgan('dev'))
 
-app.use((req,res,next)=>{
-    let time=new Date()
-    console.log(time.toLocaleTimeString())
-    next()
-})
+mongoose.connect('mongodb+srv://amitkm997:oDckR5MO7bSWa0jR@cluster0.t96wgc7.mongodb.net/')
+.then(()=>console.log("mongodb connected successfully"))
+.catch((err)=>console.log(err))
 
-app.get('/',(req,res)=>{
-    console.log(req.body)
-    const{name}=req.body;
-    if(!name) return res.send("please provide name")
-    res.send(req.body)
-})
+app.use('/',userRoute)
 
-app.get('/about',(req,res)=>{
-    res.send("this is about us page")
-})
-
-//database 
-const user=[
-    {id:1,name:"amit",profession:"Trainer"},
-    {id:2,name:"Naman",profession:"Trainer"},
-    {id:3,name:"Shivam",profession:"Trainer"},
-    {id:4,name:"jatin",profession:"Trainer"}
-]
-
-//to get data of all users
-app.get('/user',(req,res)=>{
-    res.send(user)
-})
-
-//to get data of a perticular user
-app.get('/user/:id',(req,res)=>{
-    let userId=req.params.id;
-    let userDetail=user.filter((cur)=>cur.id==userId)
-    res.send(userDetail)
-})
-
-//post a data in the user database
-app.post('/user',(req,res)=>{
-    let newUser={
-        id: user.length+1 ,
-        name:"Ritik",
-        profession:"Student"
-    }
-    user.push(newUser);
-    res.send(user)
-})
-
-//update a data of a user in the database
-app.put('/user/:id',(req,res)=>{
-    let userId=req.params.id;
-    let {name,profession}=req.body
-    let filteredUser=user.filter((cur)=>cur.id==userId)
-    filteredUser[0].name=name;
-    filteredUser[0].profession=profession;
-    res.send(user)
-})
-
-//delete a user from a data
-app.delete('/user/:id',(req,res)=>{
-    let userId=req.params.id;
-    let newUsers=user.filter((cur)=>cur.id!=userId)
-    res.send(newUsers)
-})
 app.listen(PORT,()=>{
      console.log(`server running on port ${PORT}`)
 })
+
+//oDckR5MO7bSWa0jR 
+// amitkm997
+// mongodb+srv://amitkm997:oDckR5MO7bSWa0jR@cluster0.t96wgc7.mongodb.net/
